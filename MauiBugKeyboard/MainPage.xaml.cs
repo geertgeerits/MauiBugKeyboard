4761355386592f855104048ec8bddaedff06910e
Geert Geerits <ggeerits@hotmail.com>
2024-05-29 15:30:33 +0200

    Add project files.

---
+++ MauiBugKeyboard/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiBugKeyboard
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPageLoaded(object sender, EventArgs e)
+        {
+            EntNumber.Focus();
+        }
+    }
+}
